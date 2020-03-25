875f1e79355e9adb9fa1a41335759bbf22f8cf4f
cnarr <cnarr@LAPTOP-L1JET7EF.wind3.hub>
2020-03-25 10:34:49 +0100

    fatto controller

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,8 +1,12 @@
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,6 +19,7 @@ import javafx.scene.control.TextArea;
 public class FXMLController {
 	
 	private ObservableList<String> list=FXCollections.observableArrayList();
+	private Dictionary dictionary;
 	 
 
     @FXML
@@ -47,12 +52,32 @@ public class FXMLController {
 
     @FXML
     void doClearText(ActionEvent event) {
-
+    	txtResult.setText("");
+    	txtInsert.setText("");
     }
 
     @FXML
     void doSpelleCheck(ActionEvent event) {
-
+    	long start=System.nanoTime();
+    	long end;
+    	String testo=txtInsert.getText();
+    	List<String> listTesto=new ArrayList<String>();
+    	if(testo==null)
+    		return;
+    	testo.replaceAll("[.,\\/#!$%\\*;:{}=\\-_'()\\[\\]\"]", "");
+    	dictionary.loadDictionary(choice.getValue());
+    	String[] arrayTesto=testo.split(" ");
+    	for(String s:arrayTesto) {
+    		listTesto.add(s);
+    	}
+    	List<RichWord> risultato=new ArrayList<RichWord>();
+    	risultato.addAll(dictionary.spellCheckText(listTesto));
+    	for(RichWord r:risultato) {
+    		txtResult.appendText(r+"\n");
+    	}
+    	lblErrror.setText("The text contains "+risultato.size()+" errors");
+    	end=System.nanoTime();
+    	lblTime.setText("Spellcheck completed in "+(end-start)+" seconds");
     }
 
     @FXML
@@ -68,6 +93,10 @@ public class FXMLController {
         choice.setItems(list);
 		choice.setValue("Italian");
 
+    }
+    
+    public void setDictionary(Dictionary d) {
+    	this.dictionary=d;
     }
 }
 
